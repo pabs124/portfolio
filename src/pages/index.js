d390d2d113e9b9a9a89f8f003b203b97ad842989
Sahil Pabby <pabs124@gmail.com>
2019-02-27 15:40:43 -0500

    first commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,9 +8,9 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <h1>Sahil</h1>
+    <p>Physician turned Software Developer.</p>
+    <p>Full-Stack Software Developer. Writes clean and efficient code. Works as a team member and leader.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
